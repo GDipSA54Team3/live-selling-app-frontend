8e5d9ba1c9f3422e143cecbeb2fcbb2b3631e574
jithinkrn <94780664+jithinkrn@users.noreply.github.com>
2022-08-05 16:34:58 +0800

    dashboardChanges

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+//import DashboardDataService from '../Services/DashboardDataService';
 
 class Dashboard extends Component {
 
@@ -20,14 +21,28 @@ class Dashboard extends Component {
         //   schedule: [],
         //   predictedOrders : "10-20",
         //   predictedViewers : "50-100",
-        //   OrderStatisticsByTime : null,          
+             OrderStatisticsByTime: null,              
         //   OrderStatisticsByDay: null,       
-          
-        };
+            };
     }
     componentDidMount() {
         this.retrieveRating();
-    }      
+        // this.retrieveOrderStatisticsByTime();
+        // this.retrieveOrderStatisticsByDay();
+
+    }
+    retrieveOrderStatisticsByTime() {
+        return(<div>          
+        <img src='http://127.0.0.1:5000/chartByTime' height = {300} 
+                    alt=""></img>  
+        </div>);   
+    }    
+    retrieveOrderStatisticsByDay() {
+        return(<div>          
+        <img src='http://127.0.0.1:5000/chartByDay' height = {300}
+                    alt=""></img>  
+        </div>);   
+    }
     retrieveRating() {  
         this.setState({
             rating: 4
@@ -38,38 +53,38 @@ class Dashboard extends Component {
         return (
             <div>   
                 <h2>Dashboard</h2>
-                <div class="d-flex justify-content-between">
-                    <div class="d-flex flex-column">                      
-                        <div class="p-2">
+                <div className="d-flex justify-content-between">
+                    <div className="d-flex flex-column">                      
+                        <div className="p-2">
                             <h3>My Rating</h3>                            
                             <h4>{this.state.rating}/5</h4>
                         </div>
-                        <div class="p-2">
+                        <div className="p-2">
                             <h3>My Popularity</h3>
                             <h4>Under Construction</h4>
                         </div> 
-                        <div class="p-2">
+                        <div className="p-2">
                             <h3>Orders Pending Confirmation</h3>
                             <h4>10</h4>
                         </div> 
                     </div> 
 
-                    <div class="d-flex flex-column"> 
+                    <div className="d-flex flex-column"> 
                         <div>
                             <h3>My Streaming Schedule</h3> 
                         </div>
                         <div>
-                            <p1>Table under construction</p1> 
+                            <p>Table under construction</p> 
                         </div>
 
                         <div>
                             <h3>Number of viewers & Orders Prediction</h3> 
 
-                            <div class="d-flex flex-row">
-                                <div class="p-2">
-                                <form class="d-flex flex-column">
-                                    <label class="p-2">Product Category</label>
-                                    <select class="p-2" id="inlineFormCustomSelectPref">
+                            <div className="d-flex flex-row">
+                                <div className="p-2">
+                                <form className="d-flex flex-column">
+                                    <label className="p-2">Product Category</label>
+                                    <select className="p-2" id="inlineFormCustomSelectPref">
                                         <option selected>Choose...</option>
                                         <option value="1">Clothings</option>
                                         <option value="2">Food</option>
@@ -81,10 +96,9 @@ class Dashboard extends Component {
                                         <option value="2">Sprots Items</option>
                                         <option value="3">Groceries</option>
                                         <option value="3">Others</option>
-                                    </select>
-                                
-                                    <label class="p-2" for="inlineFormCustomSelectPref">Day</label>
-                                    <select class="p-2" id="inlineFormCustomSelectPref">
+                                    </select>                                
+                                    <label className="p-2" for="inlineFormCustomSelectPref">Day</label>
+                                    <select className="p-2" id="inlineFormCustomSelectPref">
                                         <option selected>Choose...</option>
                                         <option value="1">Sun</option>
                                         <option value="2">Mon</option>
@@ -95,41 +109,38 @@ class Dashboard extends Component {
                                         <option value="1">Sat</option>                                   
                                     </select>
 
-                                    <label class="p-2" for="inlineFormCustomSelectPref">Streaming Period</label>
-                                    <select class="p-2" id="inlineFormCustomSelectPref">
+                                    <label className="p-2" for="inlineFormCustomSelectPref">Streaming Period</label>
+                                    <select className="p-2" id="inlineFormCustomSelectPref">
                                         <option selected>Choose...</option>
                                         <option value="1">12am - 6am</option>
                                         <option value="1">6am - 12pm</option>
                                         <option value="1">12pm - 6pm</option>
                                         <option value="1">6pm - 12am</option>                                    
                                     </select>
-
                                     <button type="submit" class="btn btn-primary my-1">Predict</button>
                                 </form>                                
                                 </div>
                                     <div >
                                         <h4>Expected Orders:</h4>
-                                        <p1>Under Construction</p1> 
+                                        <p>Under Construction</p> 
                                         <h4>Expected Viewers:</h4>
-                                        <p1>Under Construction</p1>
+                                        <p>Under Construction</p>
                                     </div>
                                 </div>
                             </div>                       
                     </div>
-                    <div class="d-flex flex-column"> 
+                    <div className="d-flex flex-column"> 
                         <div>
                             <h3>Platform Sales Statistics</h3> 
                         </div>
-                        <div>
-                            <h3>Graph.jpg</h3> 
-                        </div>
-                        <div>
-                            <h3>Graph.jpg</h3> 
+                        <div> 
+                       </div>   
+                         <span>{this.retrieveOrderStatisticsByTime()}</span>
+                         <span>{this.retrieveOrderStatisticsByDay()}</span>                    
                         </div>
                     </div>                           
                 <div/>                       
-                    </div>
-            </div>
+            </div>            
         );  
     } 
 }
